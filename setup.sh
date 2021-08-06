d53d4e8b8ed54cb3718ebb05bab95e19972dd835
Eggpan <eggpan@eggpan.net>
2021-08-06 23:58:54 +0900

    updated setup.sh

--- setup.sh
+++ setup.sh
@@ -3,25 +3,28 @@ set -e
 
 currentDir="$PWD"
 
-# set umask 022
-sed -i 's/^#\(umask\)/\1/' ~/.profile
+# add sudo user
+sudo grep "$USER" /etc/sudoers >/dev/null 2>/dev/null && :
+if [ $? -ne 0 ]; then
+  echo "$USER ALL=NOPASSWD: ALL" | sudo EDITOR='tee -a' visudo
+fi
 
 # set apt repositories
 sudo sed -i 's#//\(archive\.ubuntu\.com\)#//jp.\1#' /etc/apt/sources.list
 
 # install git / language-pack-ja / ansible
-sudo apt-get update && \
-sudo DEBIAN_FRONTEND=noninteractive apt-get install -y \
+sudo apt-get update \
+&& sudo DEBIAN_FRONTEND=noninteractive apt-get install -y \
   -o Dpkg::Options::="--force-confdef" \
   -o Dpkg::Options::="--force-confold" \
-  aptitude git python3-pip
-pip3 install ansible
+  git python3-pip \
+&& pip3 install ansible
 
 # clone and execute playbook
 tmpDir=$(mktemp -d)
 git clone https://github.com/eggpan/wsl-setup.git $tmpDir
 cd $tmpDir/playbook
-$HOME/.local/bin/ansible-playbook -i ,localhost --connection=local -K setup_wsl.yml
+$HOME/.local/bin/ansible-playbook -i ,localhost --connection=local setup_wsl.yml
 rm -rf $tmpDir
 
 cd "$currentDir"
